c8c9124ec6379cc0615406a45493d65b922420c9
ChaseAMarsh <camarsh7@asu.edu>
2017-10-21 22:37:01 -0700

    Update SimpleArraySum.java

--- Algorithms/Warmup/SimpleArraySum.java
+++ Algorithms/Warmup/SimpleArraySum.java
@@ -7,11 +7,11 @@ import java.util.regex.*;
 
 public class Solution {
 
-    static int simpleArraySum(int n, int[] ar) {
+    static int simpleArraySum(int n, int[] array) {
         // Complete this function
         int sum = 0;
         for(int i = 0; i < n; i++) {
-            sum += ar[i];
+            sum += array[i];
         }
         return sum;
     }
@@ -19,11 +19,11 @@ public class Solution {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        int[] ar = new int[n];
-        for(int ar_i = 0; ar_i < n; ar_i++){
-            ar[ar_i] = in.nextInt();
+        int[] array = new int[n];
+        for(int i = 0; i < n; i++){
+            array[i] = in.nextInt();
         }
-        int result = simpleArraySum(n, ar);
+        int result = simpleArraySum(n, array);
         System.out.println(result);
     }
 }
